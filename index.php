4aeda755d9364052970affadffc3b893d02e0d5f
root <root@localhost.localdomain>
2019-09-04 22:21:19 +0530

    Completed

---
+++ index.php
@@ -0,0 +1,228 @@
+<html>
+<head>
+	<title>Red Nuno Bootstrap Theme</title>
+	<meta name="author" content="Deepak Dhiman">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body data-spy="scroll" data-target="#mynav">
+<!--- home section -->
+<div id="home">
+	<!--- nav bar-->
+	<nav class="navbar navbar-dark navbar-expand-md bg-dark fixed-top">
+		<a class="navbar-brand" href="#">
+			<span class="text text-danger" style="border:solid 2px red;padding:2px 3px; margin-right:4px;">
+				<strong>R</strong>
+			</span>NUNO
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle='collapse' data-target="#mynav">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="mynav">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#home">Home</a>
+				</li>	
+				<li class="nav-item">
+					<a class="nav-link" href="#course">Course</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#features">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#resources">Resources</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#clients">clients</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#contact">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<!--nav end-->
+
+<!--- landing page-->
+	<div id="landing">
+		<div class="home-wrap">
+			<div class="home-inner">
+				<div class="caption text-center">
+					<h1>Welcome to Red Nuno</h1>
+					<h3>Web Bootstrap 4 course</h3>
+					<a href="#course" class="btn btn-outline-light ">Get Started</a>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+<!-- landing page end -->
+</div>
+<!-- home section end-->
+<div class="offset" id="course">
+	<div class="col-12 narrow text-center">
+		<h1>Red Nuno Bootstrap 4 Theme Course</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation consequat. </p>
+		<a class="btn btn-secondary" href="http://deepakdhiman.tk" target="_blank">Bootstrap Course</a>
+	</div>
+</div>
+<!-- features section-->
+<div class="offset" id="features">
+	<div class="jumbotron">
+		<div class="narrow text-center">
+			<h3 class="heading">Features</h3>
+			<div class="h_u"></div>
+		</div>
+		<div class="row text-center">
+			<div class="col-md-4 ">
+				<div class="narrow">
+					<i class="fas fa-play-circle fa-4x" data-fa-transform="shrink-3 up-3"></i>
+					<h3>Custom Animation</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+				</div>
+			</div>
+			<div class="col-md-4 ">
+				<div class="narrow">
+					<i class="fas fa-sliders-h fa-4x" data-fa-transform="shrink-4 up-3"></i>
+					<h3>Content Slider</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+				</div>
+			</div>
+			<div class="col-md-4 ">
+				<div class="narrow">
+					<i class="fab fa-wpforms fa-4x" data-fa-transform="shrink-4 up-3"></i>
+					<h3>Contact Forms</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div id="resources" class="offset">
+	<div class="fixed-back">
+		<div class="row dark text-center">
+			<div class="col-12 narrow">
+				<h3 class="heading">Built With Care</h3>
+				<div class="h_u"></div>
+			</div>
+			<div class="col-md-4">
+				<h3>HTML5</h3>
+				<div class="feature">
+					<i class="fas fa-code fa-3x"></i>
+				</div>
+				<p class="lead">Bulit with the latest,HTML 5.</p>
+			</div>
+			<div class="col-md-4">
+				<h3>BOOTSTRAP 4</h3>
+				<div class="feature">
+					<i class="fas fa-bold fa-3x"></i>
+				</div>
+				<p class="lead">Bulit with the latest,BOOTSTRAP 4.</p>
+			</div>
+			<div class="col-md-4">
+				<h3>CSS 3</h3>
+				<div class="feature">
+					<i class="fab fa-css3 fa-3x"></i>
+				</div>
+				<p class="lead">Bulit with the latest,CSS 3.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!--- client section-->
+
+<div class="offset" id="clients">
+	<div class="jumbotron">
+		<div class="col-12 narrow text-center">
+				<h3 class="heading">Clients</h3>
+				<div class="h_u"></div>
+			</div>
+		<div class="row">
+			<div class="col-md-6 client">
+				<div class="row">
+					<div class="col-md-4">
+						<img src="images/1.jpg" >
+					</div>
+					<div class="col-md-8">
+						<blockquote>
+							<i class="fas fa-quote-left"></i>
+							<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. </p>
+						</blockquote>
+						<hr class="client-hr">
+						<cite>&#8212; Deepak,Freelance Web Developer.</cite>
+					</div>
+					
+				</div>
+			</div>
+			<div class="col-md-6 client">
+				<div class="row">
+					<div class="col-md-4">
+						<img src="images/reswin.jpg" >
+					</div>
+					<div class="col-md-8">
+						<blockquote>
+							<i class="fas fa-quote-left"></i>
+							<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. </p>
+						</blockquote>
+						<hr >
+						<cite>&#8212; Reswin,Small Buisness Owner.</cite>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+	<div class="col-12 narrow text-center">
+		<p class="lead">Want to learn more about Developing bootstrap themes.</p>
+		<a href="http://deepakdhiman.tk" class="btn btn-secondary">Bootstrap Course</a>
+	</div>
+</div>
+<div class="offset" id="contact">
+	<footer class="text-center">
+		<div class="row justify-content-center">
+			<div class="col-md-5 ">
+				<b ><span class="text  text-danger" >
+				R
+				</span>NUNO</b><br>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+				<strong>Contact Info</strong>
+				<p>+918950365403</br>mpd90686@gmail.com</p>
+				<a href="#"><i class="fab fa-facebook-square"></i></a>
+				<a href="#"><i class="fab fa-twitter-square"></i></a>
+				<a href="#"><i class="fab fa-instagram"></i></a>
+			</div>
+
+		</div>
+		<hr class="socket"></hr>
+		<small>&copy;Red Nuno Theme.</small>
+	</footer>
+
+</div>
+
+
+
+
+
+</body>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/all.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</html>
